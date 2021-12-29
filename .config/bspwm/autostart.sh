b03dcb750affa512d0defaaa993b3be9767fe689
ImRayy <1mrayunlikechaser@pm.me>
2021-12-29 22:25:57 +0530

    initial push: most files copyed from arcolinuxB

---
+++ .config/bspwm/autostart.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+function run {
+  if ! pgrep $1 ;
+  then
+    $@&
+  fi
+}
+
+#Find out your monitor name with xrandr or arandr (save and you get this line)
+#xrandr --output VGA-1 --primary --mode 1360x768 --pos 0x0 --rotate normal
+#xrandr --output DP2 --primary --mode 1920x1080 --rate 60.00 --output LVDS1 --off &
+#xrandr --output LVDS1 --mode 1366x768 --output DP3 --mode 1920x1080 --right-of LVDS1
+#xrandr --output HDMI2 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
+#autorandr horizontal
+
+$HOME/.config/polybar/launch.sh &
+
+#change your keyboard if you need it
+#setxkbmap -layout be
+
+keybLayout=$(setxkbmap -v | awk -F "+" '/symbols/ {print $2}')
+
+if [ $keybLayout = "be" ]; then
+  run sxhkd -c ~/.config/bspwm/sxhkd/sxhkdrc-azerty &
+else
+  run sxhkd -c ~/.config/bspwm/sxhkd/sxhkdrc &
+fi
+
+#Some ways to set your wallpaper besides variety or nitrogen
+#feh --bg-scale ~/.config/bspwm/wall.png &
+#feh --bg-fill /usr/share/backgrounds/arcolinux/arco-wallpaper.jpg &
+#feh --randomize --bg-fill ~/Képek/*
+#feh --randomize --bg-fill ~/Dropbox/Apps/Desktoppr/*
+nitrogen --restore &
+#dex $HOME/.config/autostart/arcolinux-welcome-app.desktop
+#xsetroot -cursor_name left_ptr &
+
+#conky -c $HOME/.config/bspwm/system-overview &
+#run variety &
+#run nm-applet &
+#run pamac-tray &
+#run xfce4-power-manager &
+numlockx on &
+#blueberry-tray &
+picom --experimental-backend &
+#/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+#/usr/lib/xfce4/notifyd/xfce4-notifyd &
+#run volumeicon &
+#nitrogen --restore &
+#run caffeine &
+#run vivaldi-stable &
+#run firefox &
+#run thunar &
+#run dropbox &
+#run insync start &
+#run discord &
+#run spotify &
+#run atom &
