6e02bca7848780c4b84595d833a9f5ace5c7350f
ImRayy <1mrayunlikechaser@pm.me>
2022-01-11 01:42:04 +0530

    bspwm: major changes

--- .config/bspwm/scripts/autostart.sh
+++ .config/bspwm/scripts/autostart.sh
@@ -7,25 +7,22 @@ function run {
   fi
 }
 
-#Find out your monitor name with xrandr or arandr (save and you get this line)
-#xrandr --output VGA-1 --primary --mode 1360x768 --pos 0x0 --rotate normal
-#xrandr --output DP2 --primary --mode 1920x1080 --rate 60.00 --output LVDS1 --off &
-#xrandr --output LVDS1 --mode 1366x768 --output DP3 --mode 1920x1080 --right-of LVDS1
-#xrandr --output HDMI2 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
-#autorandr horizontal
+# setting rr to 75
+xrandr -s 1920x1080 -r 75.00
 
-$HOME/.config/polybar/launch.sh &
+~/.config/polybar/forest/launch.sh &
 
 #change your keyboard if you need it
 #setxkbmap -layout be
 
 keybLayout=$(setxkbmap -v | awk -F "+" '/symbols/ {print $2}')
 
-if [ $keybLayout = "be" ]; then
-  run sxhkd -c ~/.config/bspwm/sxhkd/sxhkdrc-azerty &
-else
-  run sxhkd -c ~/.config/bspwm/sxhkd/sxhkdrc &
-fi
+#if [ $keybLayout = "be" ]; then
+#  run sxhkd -c ~/.config/bspwm/sxhkd/sxhkdrc-azerty &
+#else
+#  run sxhkd -c ~/.config/bspwm/sxhkd/sxhkdrc &
+#fi
+run sxhkd -c ~/.config/bspwm/sxhkd/sxhkdrc &
 
 #Some ways to set your wallpaper besides variety or nitrogen
 #feh --bg-scale ~/.config/bspwm/wall.png &
@@ -42,12 +39,12 @@ nitrogen --restore &
 #run pamac-tray &
 #run xfce4-power-manager &
 numlockx on &
-#blueberry-tray &
-picom --experimental-backend &
+run copyq &
+picom --experimental-backend --config ~/.config/bspwm/picom/picom.conf &
+/usr/lib/polkit-kde-authentication-agent-1 &
 #/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
-#/usr/lib/xfce4/notifyd/xfce4-notifyd &
+/usr/lib/xfce4/notifyd/xfce4-notifyd &
 #run volumeicon &
-#nitrogen --restore &
 #run caffeine &
 #run vivaldi-stable &
 #run firefox &
