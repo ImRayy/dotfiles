657836f4aba562c94ed07d47ad4a98b3ae12213a
ImRayy <souradeep20roy01@gmail.com>
2022-10-24 19:13:36 +0530

    update alisa.fish

--- .config/fish/completions/alias.fish
+++ .config/fish/completions/alias.fish
@@ -1,6 +1,6 @@
 ### ALIASES ###
-#\x1b[2J   <- clears tty
-#\x1b[1;1H <- goes to (1, 1) (start)
+# \x1b[2J   <- clears tty
+# \x1b[1;1H <- goes to (1, 1) (start)
 
 # Basic
 alias c=clear
@@ -13,8 +13,9 @@ alias cd3="cd ../../.."
 alias cd4="cd ../../../.."
 alias cd5="cd ../../../../.."
 
+#file managerment
 alias ls="lsd --group-dirs first"
-
+alias lf='~/.config/lf/scripts/lfrun'
 
 # Pacman
 alias pacq="pacman -Q | grep"
@@ -41,6 +42,13 @@ alias yayreps="yay -Ss"
 alias yaycache="yay -Sc"
 alias yayremdep="yay -Yc"
 
+# Paru
+alias pain="paru -S"
+alias parem="paru -Rns"
+alias paupd='paru -Sy'
+alias paupg='paru -Sua'
+alias parem='paru -Rns'
+alias paorph='paru -Qtd'
 
 # Pamac
 alias pin="pamac build"
@@ -50,24 +58,11 @@ alias psearch="pamac search"
 # NoteEditor
 alias nvide=neovide
 
-# Paru
-alias pain="paru -S"
-alias parem="paru -Rns"
-alias paupd='paru -Sy'
-alias paupg='paru -Sua'
-alias parem='paru -Rns'
-alias paorph='paru -Qtd'
+#grub
+alias grub-update="sudo grub-mkconfig -o /boot/grub/grub.cfg"
 
 # youtube-dlp
 alias ytaudio="yt-dlp -x --audio-format mp3"
-
-# Others
-alias py=python		
-alias gt=gotop		
-alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
-alias sync="bash /home/imray/sync.sh"
-alias cat=bat
-alias ka=killall
 
 # Copy using rsync [ with progress bar ]
 alias rcp="rsync -r -avz --progress"
@@ -85,27 +80,32 @@ alias trl="trash-list"
 alias trr="trash-restore"
 alias tre="trash-empty"
 
+# Fetch 
+alias of='onefetch'
+alias pf='pfetch'
+alias nf='neofetch'
+
+# Exif cleaner 
+alias rmmetad='exiftool -all= -overwrite_original'
+alias rmametad='exiftool -all= -overwrite_original -ext'
+
+# nmcli
+alias won='nmcli radio wifi on'
+alias woff='nmcli radio wifi off'
+
 # Other programs
 alias driver='glxinfo | grep OpenGL'
 alias csend='croc send --code'
 alias scrcpylbr='scrcpy --bit-rate 2M'
-alias rmmetad='exiftool -all= -overwrite_original'
-alias rmametad='exiftool -all= -overwrite_original -ext'
-alias won='nmcli radio wifi on'
-alias woff='nmcli radio wifi off'
 alias cdns='sudo nvim /etc/resolv.conf'
-alias matrix='unimatrix'
 alias na='nm-applet'
 alias anime='~/Downloads/GIT/ani-cli/ani-cli'
 alias g='greenclip'
 alias gp='greenclip print'
 alias music='ncmpcpp'
-alias of='onefetch'
-alias pf='pfetch'
-alias nf='neofetch'
-alias aquarium=asciiquarium 
-alias lf='~/.config/lf/scripts/lfrun'
 alias updmirror="cp /etc/pacman.d/mirrorlist ~/backup && cd ~/backup && mv mirrorlist mirrorlist_(date "+%Y-%m-%d-%T").bak && cd $HOME && sudo reflector --latest 20 --age 1 --fastest 5 --protocol https --sort rate --save /etc/pacman.d/mirrorlist && echo 'mirrorlist successfully updated'"
-
-# GRUB
-alias grubupd='sudo grub-mkconfig -o /boot/grub/grub.cfg '
+alias py=python3
+alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+alias cat=bat
+alias ka=killall
+alias printc='python3 ~/Mini-Projects/Scripts/easy_print/evenodd_typer.py'
