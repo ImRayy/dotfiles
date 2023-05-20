f04cb1f7b5851e22f0ace82bc77095ff36c7cd8f
ImRayy <souradeep20roy01@gmail.com>
2023-05-20 23:59:47 +0530

    Updated dashboard theme

--- .config/nvim/lua/plugins/configs/dashboard.lua
+++ .config/nvim/lua/plugins/configs/dashboard.lua
@@ -4,7 +4,9 @@ if not status_ok then
 end
 
 local ascii = {
-
+	"",
+	"",
+	"",
 	"          ▀████▀▄▄              ▄█ ",
 	"            █▀    ▀▀▄▄▄▄▄    ▄▄▀▀█ ",
 	"    ▄        █          ▀▀▀▀▄  ▄▀  ",
@@ -15,41 +17,64 @@ local ascii = {
 	"    █  ▄▀  █    ▀██▀    ▀▀ ▀▀  ▄▀  ",
 	"   █   █  █      ▄▄           ▄▀   ",
 	"",
+	"",
+	" [ TIP: To exit NeoVim, switch off MCB of your house. ] ",
+	"",
+	"",
+}
+
+local footer_text = {
+	"",
 }
 
 vim.g.indent_blankline_filetype_exclude = { "dashboard" }
 vim.g.indent_blankline_buftype_exclude = { "dashboard" }
 
 dashboard.setup({
-	theme = "hyper",
+	theme = "doom",
 	config = {
 		header = ascii,
-		shortcut = {
+		center = {
 			{
-				desc = " Update",
-				group = "@property",
-				action = "Lazy update",
+				icon = "  ",
+				icon_hl = "Title",
+				desc = "Update Plugins                                       ",
 				key = "u",
+				action = "PackerUpdate",
 			},
 			{
-				desc = " Files",
-				icon_hl = "@variable",
-				group = "Label",
-				action = "Telescope find_files",
+				icon = "  ",
+				icon_hl = "Title",
+				desc = "Find Files",
 				key = "f",
+				keymap = "SPC f f",
+				action = "Telescope find_files",
 			},
 			{
-				desc = " Bookmarks",
-				group = "DiagnosticHint",
-				action = "Telescope marks",
+				icon = "  ",
+				icon_hl = "Title",
+				desc = "Recent Files",
+				key = "r",
+				keymap = "SPC f r",
+				action = "Telescope oldfiles",
+			},
+			{
+				icon = "  ",
+				icon_hl = "Title",
+				desc = "Bookmarks",
 				key = "b",
+				keymap = "SPC f b",
+				action = "Telescope marks",
 			},
 			{
-				desc = " Projects",
-				group = "Number",
-				action = "SearchSession",
-				key = "p",
+				icon = "  ",
+				icon_hl = "Title",
+				desc = "Change theme",
+				key = "t",
+				keymap = "SPC h t",
+				action = "Telescope marks",
 			},
 		},
+		-- footer = footer_text,
 	},
 })
