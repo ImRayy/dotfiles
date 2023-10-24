179ac4dea9b22a76f2f52a43a25054642f9f4c41
RAY <souradeep20roy01@gmail.com>
2023-10-24 12:56:52 +0530

    "LF: Updated set_wall script"

--- .config/lf/scripts/set_wall.sh
+++ .config/lf/scripts/set_wall.sh
@@ -1,12 +1,22 @@
-#!/bin/bash
-
+#!/usr/bin/env bash
 log_path="/home/${USER}/.config/wall.txt"
 
 wallpaper_path() {
     while read -r line; do
         echo "$line" && down="$line"
-
     done <"$log_path" | tail -n1
 }
 
-swww img $(wallpaper_path)
+set_wall() {
+    swww img "$(wallpaper_path)" \
+        --transition-fps 60 \
+        --transition-duration 2 \
+        --transition-type left \
+        --transition-bezier .42,0,.58,0.6
+}
+
+if ! pgrep -x swww-daemon > /dev/null; then
+    swww init && set_wall
+else
+    set_wall
+fi
