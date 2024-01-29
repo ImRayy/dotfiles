3dcb089a880e58ad99c3e2067b2e3576fc722c09
RAY <souradeep_roy@pm.me>
2024-01-29 23:03:53 +0530

    Rofi: Added powermenu
    Big props to adi1090x for the config! I tweaked it a bit to fit my style

---
+++ .config/rofi/powermenu/launch.sh
@@ -0,0 +1,95 @@
+#!/usr/bin/env bash
+
+# Current Theme
+dir="$HOME/.config/rofi/powermenu/"
+theme='config'
+
+# Options
+shutdown=' Shutdown'
+reboot=' Reboot'
+lock=' Lock'
+logout=' Logout'
+yes=' Yes'
+no=' No'
+
+# CMDs
+user=$(whoami)
+hostname=$(hostname)
+
+# Rofi CMD
+rofi_cmd() {
+	rofi -dmenu \
+        -mesg "${user} on ${hostname}" \
+		-theme ${dir}/${theme}.rasi
+}
+
+# Confirmation CMD
+confirm_cmd() {
+	rofi -theme-str 'window {location: center; anchor: center; fullscreen: false; width: 250px;}' \
+		-theme-str 'mainbox {children: [ "message", "listview" ];}' \
+		-theme-str 'listview {columns: 2; lines: 1;}' \
+		-theme-str 'element-text {horizontal-align: 0.5;}' \
+		-theme-str 'textbox {horizontal-align: 0.5;}' \
+		-dmenu \
+		-p 'Confirmation' \
+		-mesg 'Are you Sure?' \
+		-theme ${dir}/${theme}.rasi
+}
+
+# Ask for confirmation
+confirm_exit() {
+	echo -e "$yes\n$no" | confirm_cmd
+}
+
+# Pass variables to rofi dmenu
+run_rofi() {
+	echo -e "$lock\n$logout\n$reboot\n$shutdown" | rofi_cmd
+}
+
+# Execute Command
+run_cmd() {
+	selected="$(confirm_exit)"
+	if [[ "$selected" == "$yes" ]]; then
+		if [[ $1 == '--shutdown' ]]; then
+			systemctl poweroff
+		elif [[ $1 == '--reboot' ]]; then
+			systemctl reboot
+		elif [[ $1 == '--suspend' ]]; then
+			mpc -q pause
+			amixer set Master mute
+			systemctl suspend
+		elif [[ $1 == '--logout' ]]; then
+			if [[ "$DESKTOP_SESSION" == 'hyprland' ]]; then
+                # why pkill? killall Hyprland dosen't work on nixos.
+                # why killall or pkill? cause 'hyprctl disatch exit 0' 
+                # causes system shutdown for some reason.
+				pkill -9 Hyprland  
+			elif [[ "$DESKTOP_SESSION" == 'bspwm' ]]; then
+				bspc quit
+			fi
+		fi
+	else
+		exit 0
+	fi
+}
+
+# Actions
+chosen="$(run_rofi)"
+case ${chosen} in
+    $shutdown)
+		run_cmd --shutdown
+        ;;
+    $reboot)
+		run_cmd --reboot
+        ;;
+    $lock)
+        if [[ "$XDG_SESSION_TYPE" == 'wayland' ]]; then
+			swaylock
+        else 
+			betterlockscreen -l
+		fi
+        ;;
+    $logout)
+		run_cmd --logout
+        ;;
+esac
